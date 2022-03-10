2be9336f3c5fa661ea1f8af15546230937b41848
sharma-sourav <souravsharma8195930242@gmail.com>
2022-03-10 20:22:02 +0530

    basic file_manager

---
+++ main.py
@@ -0,0 +1,15 @@
+#print("hello")
+# print(20+""
+# print("hello",12,'a')
+# print(10>5)
+# print(10>15)
+# variable in python
+# a=10
+# print(a)
+# found the id address of no
+# a=10
+# b=20
+# print(a,b)
+# print(id(a),id(b))
+
+# how is string concatenation in python
